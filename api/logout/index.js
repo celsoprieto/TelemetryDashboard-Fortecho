2ed368a7245ffbacbdb3b16e2fc565d086ff9159
celsoprieto <celsoprieto@hotmail.com>
2026-02-25 16:37:43 +0100

    remove all

--- api/logout/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context) {
-    context.res = {
-        status: 302,
-        headers: {
-            "Set-Cookie": "auth_token=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0",
-            "Location": "/"
-        }
-    };
-};
